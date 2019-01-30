75e17def2091909faeffbcd9ba477268ab655192
Violet Wei <weizhenzhou@weizhenzhoudeMacBook-Air.local>
2019-01-30 16:15:47 -0500

    Initial commit of the backend application

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplication.java
@@ -0,0 +1,20 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class EventRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+}
