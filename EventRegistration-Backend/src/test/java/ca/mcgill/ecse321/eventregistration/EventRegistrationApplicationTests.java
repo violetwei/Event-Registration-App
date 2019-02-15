d60f866ccbc23cd25fc16e6ab827aa9741e9e564
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-02-15 15:51:45 -0500

    Update EventRegistrationApplicationTests.java

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -1,5 +1,9 @@
 package ca.mcgill.ecse321.eventregistration;
 
+import java.sql.Date;
+import java.sql.Time;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
