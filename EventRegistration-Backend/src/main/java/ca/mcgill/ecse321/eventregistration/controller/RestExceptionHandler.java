5dc69d6fd734f27ac7764bd1ed666db8030aa135
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-03-13 13:04:44 -0400

    Completed until end of Tutorial 6

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/RestExceptionHandler.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse321.eventregistration.controller;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		String error = "Malformed JSON request";
+		return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex));
+	}
+
+	private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
+
+	@ExceptionHandler(IllegalArgumentException.class)
+	protected ResponseEntity<Object> handleEntityNotFound(IllegalArgumentException ex) {
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST); // This is more common in practice than NOT_FOUND
+		apiError.setMessage(ex.getMessage());
+		return buildResponseEntity(apiError);
+	}
+}
