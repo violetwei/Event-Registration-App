8a6cdcf3982c805baca357322a7275e29fa48b10
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-03-13 13:03:30 -0400

    Completed end of Tutorial 6

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/controller/EventRegistrationRestController.java
@@ -0,0 +1,155 @@
+package ca.mcgill.ecse321.eventregistration.controller;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.eventregistration.dto.EventDto;
+import ca.mcgill.ecse321.eventregistration.dto.PersonDto;
+import ca.mcgill.ecse321.eventregistration.dto.RegistrationDto;
+import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Person;
+import ca.mcgill.ecse321.eventregistration.model.Registration;
+import ca.mcgill.ecse321.eventregistration.service.EventRegistrationService;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class EventRegistrationRestController {
+
+	@Autowired
+	private EventRegistrationService service;
+
+	// @formatter:off
+	// Turning off formatter here to ease comprehension of the sample code by
+	// keeping the linebreaks
+	// Example REST call:
+	// http://localhost:8088/persons/John
+	@PostMapping(value = { "/persons/{name}", "/persons/{name}/" })
+	public PersonDto createPerson(@PathVariable("name") String name) throws IllegalArgumentException {
+		// @formatter:on
+		Person person = service.createPerson(name);
+		return convertToDto(person);
+	}
+
+	// @formatter:off
+	// Example REST call:
+	// http://localhost:8080/events/testevent?date=2013-10-23&startTime=00:00&endTime=23:59
+	@PostMapping(value = { "/events/{name}", "/events/{name}/" })
+	public EventDto createEvent(@PathVariable("name") String name, @RequestParam Date date,
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern = "HH:mm") LocalTime startTime,
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.TIME, pattern = "HH:mm") LocalTime endTime)
+			throws IllegalArgumentException {
+		// @formatter:on
+		Event event = service.createEvent(name, date, Time.valueOf(startTime), Time.valueOf(endTime));
+		return convertToDto(event);
+	}
+
+	@GetMapping(value = { "/persons", "/persons/" })
+	public List<PersonDto> getAllPersons() {
+		List<PersonDto> persons = new ArrayList<>();
+		for (Person person : service.getAllPersons()) {
+			persons.add(convertToDto(person));
+		}
+		return persons;
+	}
+
+	@GetMapping(value = { "/events", "/events/" })
+	public List<EventDto> getAllEvents() {
+		List<EventDto> eventDtos = new ArrayList<>();
+		for (Event event : service.getAllEvents()) {
+			eventDtos.add(convertToDto(event));
+		}
+		return eventDtos;
+	}
+
+	// @formatter:off
+	@PostMapping(value = { "/register", "/register/" })
+	public RegistrationDto registerPersonForEvent(@RequestParam(name = "person") PersonDto pDto,
+			@RequestParam(name = "event") EventDto eDto) throws IllegalArgumentException {
+		// @formatter:on
+
+		// Both the person and the event are identified by their names
+		Person p = service.getPerson(pDto.getName());
+		Event e = service.getEvent(eDto.getName());
+
+		Registration r = service.register(p, e);
+		return convertToDto(r, p, e);
+	}
+
+	// Example REST call:
+	// http://localhost:8088/registrations/person/JohnDoe
+	@GetMapping(value = { "/registrations/person/{name}", "/registrations/person/{name}/" })
+	public List<EventDto> getEventsOfPerson(@PathVariable("name") PersonDto pDto) {
+		Person p = convertToDomainObject(pDto);
+		return createEventDtosForPerson(p);
+	}
+
+	@GetMapping(value = { "/persons/{name}", "/persons/{name}/" })
+	public PersonDto getPersonByName(@PathVariable("name") String name) throws IllegalArgumentException {
+		return convertToDto(service.getPerson(name));
+	}
+
+	@GetMapping(value = { "/events/{name}", "/events/{name}/" })
+	public EventDto getEventByName(@PathVariable("name") String name) throws IllegalArgumentException {
+		return convertToDto(service.getEvent(name));
+	}
+
+	// Model - DTO conversion methods (not part of the API)
+
+	private EventDto convertToDto(Event e) {
+		if (e == null) {
+			throw new IllegalArgumentException("There is no such Event!");
+		}
+		EventDto eventDto = new EventDto(e.getName(),e.getDate(),e.getStartTime(),e.getEndTime());
+		return eventDto;
+	}
+
+	private PersonDto convertToDto(Person p) {
+		if (p == null) {
+			throw new IllegalArgumentException("There is no such Person!");
+		}
+		PersonDto personDto = new PersonDto(p.getName());
+		personDto.setEvents(createEventDtosForPerson(p));
+		return personDto;
+	}
+
+	private RegistrationDto convertToDto(Registration r, Person p, Event e) {
+		EventDto eDto = convertToDto(e);
+		PersonDto pDto = convertToDto(p);
+		return new RegistrationDto(pDto, eDto);
+	}
+
+	private Person convertToDomainObject(PersonDto pDto) {
+		List<Person> allPersons = service.getAllPersons();
+		for (Person person : allPersons) {
+			if (person.getName().equals(pDto.getName())) {
+				return person;
+			}
+		}
+		return null;
+	}
+
+	// Other extracted methods (not part of the API)
+
+	private List<EventDto> createEventDtosForPerson(Person p) {
+		List<Event> eventsForPerson = service.getEventsAttendedByPerson(p);
+		List<EventDto> events = new ArrayList<>();
+		for (Event event : eventsForPerson) {
+			events.add(convertToDto(event));
+		}
+		return events;
+	}
+
+}
